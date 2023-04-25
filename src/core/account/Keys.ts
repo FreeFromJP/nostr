0b57e769349b64c4cdd44778403d828dae207a24
Ungigdu <Ungigdu@protonmail.com>
2023-04-25 21:51:10 +0900

    add sign function to keys

--- src/core/account/Keys.ts
+++ src/core/account/Keys.ts
@@ -1,33 +1,42 @@
-import {generatePrivateKey, getPublicKey, nip19} from "nostr-tools"
+import { generatePrivateKey, getEventHash, getPublicKey, nip19, Event, signEvent } from "nostr-tools"
 
 export const PUBKEY_PREFIX = "npub"
 export const PRIKEY_PREFIX = "nsec"
 
 export class keys {
     pubkey: string
-    prikey: string
+    privkey: string
     //npub or nsec
-    constructor(keyString?:string) {
-        if(keyString != null) {
-            if(keyString.length != 63) throw "key length invalid"
-            if(keyString.startsWith(PUBKEY_PREFIX)){
+    constructor(keyString?: string) {
+        if (keyString != null) {
+            if (keyString.length != 63) throw "key length invalid"
+            if (keyString.startsWith(PUBKEY_PREFIX)) {
                 this.pubkey = nip19.decode(keyString).data as string
-                this.prikey = ""
-            }else if (keyString.startsWith(PRIKEY_PREFIX)){
-                this.prikey = nip19.decode(keyString).data as string
-                this.pubkey = getPublicKey(this.prikey)
-            }else{
+                this.privkey = ""
+            } else if (keyString.startsWith(PRIKEY_PREFIX)) {
+                this.privkey = nip19.decode(keyString).data as string
+                this.pubkey = getPublicKey(this.privkey)
+            } else {
                 throw "undefine key format"
             }
-        }else{
+        } else {
             //generate key for new user
-            this.prikey = generatePrivateKey()
-            this.pubkey = getPublicKey(this.prikey)
+            this.privkey = generatePrivateKey()
+            this.pubkey = getPublicKey(this.privkey)
         }
     }
 
     canSign() {
-        return this.prikey != ""
+        return this.privkey != ""
     }
 
+    async sign(event: Event) {
+        event.id = getEventHash(event)
+        if (this.canSign()) {
+            event.sig = signEvent(event, this.privkey)
+        } else {
+            throw new Error('cannot sign')
+        }
+        return event
+    }
 }
