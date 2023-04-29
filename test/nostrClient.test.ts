75a23c2853890545ad689567e9de38b04f0b257d
Ungigdu <Ungigdu@protonmail.com>
2023-04-29 13:23:51 +0900

    chore: test fetch profiles

--- test/nostrClient.test.ts
+++ test/nostrClient.test.ts
@@ -15,13 +15,26 @@ describe('NostrClient', () => {
     })
 
     it('fetch()', async () => {
+        // const events = await pool.fetch([
+        //     {
+        //         authors: [settings.pubkey],
+        //         kinds: [1],
+        //         limit: 2,
+        //     },
+        // ])
+
         const events = await pool.fetch([
             {
-                authors: [settings.pubkey],
-                kinds: [1],
-                limit: 2,
+                kinds: [0],
+                authors: [
+                    '32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245',
+                    '69dfcf1cf5be81090d9d95314ffb81e0230b9f569d350cb2babe608d4faaf3b0',
+                ],
             },
         ])
+
+        console.log('events:', events)
+        console.log('event length:', events.length)
 
         // the actual received number will be greater than 2, but there will be no duplicates
         expect(events.length).toEqual(
