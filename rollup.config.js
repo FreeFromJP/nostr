c1414217fc317da84fd7c35a917fe59889a226dd
eospeng <eospeng@gmail.com>
2023-05-01 20:07:05 +0000

    build: build的时候自动移除dist目录

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,28 @@
 /* eslint no-use-before-define: 0 */
-import typescript from '@rollup/plugin-typescript';
-import {uglify} from 'rollup-plugin-uglify'
+import typescript from '@rollup/plugin-typescript'
+import { uglify } from 'rollup-plugin-uglify'
+import del from 'rollup-plugin-delete'
 
 export default {
-  input: 'src/index.ts',
-  output: {
-      file: 'dist/index.js',
-      format: 'es'
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.js',
+        format: 'es',
     },
     external: ['nostr-tools'],
-  plugins: [typescript({
-    module: 'esnext',
-    exclude: ['node_modules/**', 'test/*', 'testHelper/*']
-   }), uglify({
-     compress: {
-       drop_console: true
-      },
-    output: {
-      beautify: true,
-    }
-   })]
-};
+    plugins: [
+        typescript({
+            module: 'esnext',
+            exclude: ['node_modules/**', 'test/*', 'testHelper/*'],
+        }),
+        uglify({
+            compress: {
+                drop_console: true,
+            },
+            output: {
+                beautify: true,
+            },
+        }),
+        del({ targets: 'dist/*' }),
+    ],
+}
