ac2a786d0294f10d687081ca504751b33d2e1b2a
eosxx <eospeng@gmail.com>
2023-05-03 09:32:13 +0000

    test: add other key

--- testHelper/settings.ts
+++ testHelper/settings.ts
@@ -4,4 +4,9 @@ export const settings = {
     privkeyEncoded: 'nsec1kk2tp8qw73rydtdnngn0arlsxjege84ln56j82h5ssppkv37eass4lcsjd',
     prikey: 'b594b09c0ef44646adb39a26fe8ff034b28c9ebf9d3523aaf484021b323ecf61',
     relays: ['wss://freerelay.xyz', 'wss://relay.damus.io', 'wss://nostr-pub.wellorder.net', 'wss://nos.lol'],
+
+    pubkeyEncoded2: 'npub124lqmv5v283f7ne6hux3p4kjgx6qve6suhj3g2qvgcqyfcdgu4vs8mvwqv',
+    pubkey2: '557e0db28c51e29f4f3abf0d10d6d241b4066750e5e514280c460044e1a8e559',
+    privkeyEncoded2: 'nsec1786wk3lclxae40kt6fp5npq74u80qfn5v7dhnyljsv7d4cw4pv8q80hzgx',
+    prikey2: 'f1f4eb47f8f9bb9abecbd24349841eaf0ef02674679b7993f2833cdae1d50b0e',
 }
