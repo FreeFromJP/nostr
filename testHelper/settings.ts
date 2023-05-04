cf1aaae9dd3ded11468eaf6d9f2d189d350c5d2d
eosxx <eospeng@gmail.com>
2023-05-04 02:33:58 +0000

    test(testHelper): remove relay which can not accept filter

--- testHelper/settings.ts
+++ testHelper/settings.ts
@@ -3,7 +3,7 @@ export const settings = {
     pubkey: 'f7c1fb977450392671ab594f355f28c393c7b45594b0df61dab208cd6f7a75e3',
     privkeyEncoded: 'nsec1kk2tp8qw73rydtdnngn0arlsxjege84ln56j82h5ssppkv37eass4lcsjd',
     prikey: 'b594b09c0ef44646adb39a26fe8ff034b28c9ebf9d3523aaf484021b323ecf61',
-    relays: ['wss://freerelay.xyz', 'wss://relay.damus.io', 'wss://nostr-pub.wellorder.net', 'wss://nos.lol'],
+    relays: ['wss://freerelay.xyz', 'wss://relay.damus.io', 'wss://nostr-pub.wellorder.net'],
 
     pubkeyEncoded2: 'npub124lqmv5v283f7ne6hux3p4kjgx6qve6suhj3g2qvgcqyfcdgu4vs8mvwqv',
     pubkey2: '557e0db28c51e29f4f3abf0d10d6d241b4066750e5e514280c460044e1a8e559',
