663971a07e1ad340bfaa0f30b8728e2d1a13f74f
Ungigdu <Ungigdu@protonmail.com>
2023-05-15 16:19:51 +0900

    chore: just test

--- test/eventFetcher.test.ts
+++ test/eventFetcher.test.ts
@@ -1,19 +1,19 @@
 import { NostrClient } from 'src/index'
-import { fetchProfiles } from 'src/periphery/eventFetcher'
+import { fetchFollowersAMAP } from 'src/periphery/eventFetcher'
 import WebSocket from 'ws'
 
 import { settings } from '../testHelper/settings'
 
 global.WebSocket = WebSocket as any
 
-test('Test fetch profiles', async () => {
-    const client = new NostrClient(settings.relays)
-    const pubkeys = ['npub1kurad0nlm8xfuxhws05pcwv5z4k0ea6da4dsjygexr77a666pssqsftsm7']
-    const results = await fetchProfiles(client, pubkeys)
-    console.log(results, Object.keys(results).length)
-    client.close()
-    expect(pubkeys.length).toBe(Object.keys(results).length)
-})
+// test('Test fetch profiles', async () => {
+//     const client = new NostrClient(settings.relays)
+//     const pubkeys = ['npub1kurad0nlm8xfuxhws05pcwv5z4k0ea6da4dsjygexr77a666pssqsftsm7']
+//     const results = await fetchProfiles(client, pubkeys)
+//     console.log(results, Object.keys(results).length)
+//     client.close()
+//     expect(pubkeys.length).toBe(Object.keys(results).length)
+// })
 
 // test('Test fetch contact', async () => {
 //     const client = new NostrClient(settings.relays)
@@ -22,28 +22,27 @@ test('Test fetch profiles', async () => {
 //     client.close()
 // })
 
-// test('Test fetch contacts like me', async () => {
-//     const client = new NostrClient(settings.relays)
-//     const set = new Set<string>()
-//     const results = await fetchFollowersAMAP(
-//         client,
-//         '3efdaebb1d8923ebd99c9e7ace3b4194ab45512e2be79c1b7d68d9243e0d2681',
-//         (contacts) => {
-//             console.log(contacts.length)
-//             contacts.forEach((x) => {
-//                 set.add(x.pubkeyRaw as string)
-//             })
-//         },
-//         50, //recommend 3
-//         1000, // recmmend null
-//     )
+test('Test fetch contacts like me', async () => {
+    const client = new NostrClient(settings.relays)
+    const set = new Set<string>()
+    const results = await fetchFollowersAMAP(
+        client,
+        '6961db6aec05b27aa1db28b96b0130e6805c357e47d731533ec4ac97e5fcbebb',
+        (contacts) => {
+            console.log(contacts.length)
+            contacts.forEach((x) => {
+                set.add(x.pubkeyRaw as string)
+            })
+        },
+        1, //recommend 3
+        // recmmend null
+    )
 
-//     console.log('length added up:', results.length)
+    console.log('length added up:', results.length)
 
-//     for (const r of results) {
-//         set.add(r.pubkeyRaw as string)
-//     }
-//     console.log('actual record:', set.size)
-//     client.close()
-//     fs.writeFileSync('./test.test', JSON.stringify(Array.from(set)))
-// }, 5000000)
+    for (const r of results) {
+        set.add(r.pubkeyRaw as string)
+    }
+    console.log('actual record:', set.size)
+    client.close()
+}, 5000000)
